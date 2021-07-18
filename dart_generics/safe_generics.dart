be40e6a63c097a147581554944031a431abe17ff
Anish-D <anishd2912@gmail.com>
2021-07-18 19:27:24 +0530

    improved safe_generics for better readability

--- dart_generics/safe_generics.dart
+++ dart_generics/safe_generics.dart
@@ -3,7 +3,7 @@
  */
 main(List<String> args) {
   List<String> avengerNames = ["Hulk", "Captain America"];
-  avengerNames.add(
-      1); // gives Error: The argument type 'int' can't be assigned to the parameter type 'String'
+  //avengerNames.add(
+  //    1); // gives Error: The argument type 'int' can't be assigned to the parameter type 'String'
   print("Avenger names : $avengerNames");
 }
